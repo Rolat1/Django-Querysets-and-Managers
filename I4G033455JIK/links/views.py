61665a2be436ba09b09de3e8f9637e18c8b11d21
Rolat1 <lawaloyinkansol01@gmail.com>
2022-07-19 14:47:25 +0100

    initial commit

---
+++ I4G033455JIK/links/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from .models import Link
+from .serializers import LinkSerializer
+
+
+# Create your views here.
+from django.utils import timezone
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
+
+import datetime
+
+
+class ActiveLinkView(APIView):
+    """
+    Returns a list of all active (publicly accessible) links
+    """
+
+    def get(self, request):
+        """
+        Invoked whenever a HTTP GET Request is made to this view
+        """
+        qs = Link.public.all()
+        data = serializers.LinkSerializer(qs, many=True).data
+        return Response(data, status=status.HTTP_200_OK)
+
+
+class RecentLinkView(APIView):
+    """
+    Returns a list of recently created active links
+    """
+
+    def get(self, request):
+        """
+        Invoked whenever a HTTP GET Request is made to this view
+        """
+        seven_days_ago = timezone.now() - datetime.timedelta(days=7)
+        qs = Link.public.filter(created_date__gte=seven_days_ago)
+        data = serializers.LinkSerializer(qs, many=True).data
+        return Response(data, status=status.HTTP_200_OK)
+
+
+class PostListApi(ListAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostCreateApi(CreateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostDetailApi(RetrieveAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostUpdateApi(UpdateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostDeleteApi(DestroyAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
